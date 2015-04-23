6be3e07fadff32a962a782aae259282798f4790c
Nico Gonzalez <ngonza14@uncc.edu>
2015-04-23 02:10:43 -0400

    Finished implementation of sorting methods
    -Fixed bugs
    -Ran minor tests
    -Finish implementing all sorting methods required.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	int[] test={1,15,32,9,8,5,19};
+	int[] test={1,90,3,16,1,15,32,9,8,81,5,19,2};
     Integer[] test1= {1,15,32,9,8,5,19};
         Main t=new Main();
-        System.out.println(Arrays.toString(t.bubbleSort(test)));
-        System.out.println(Arrays.toString(t.selectionSort(test)));
-        System.out.println(Arrays.toString(t.insertionSort(test)));
-        System.out.println(t.mergeSort(test1));
+        //System.out.println(Arrays.toString(t.bubbleSort(test)));//working
+        //System.out.println(Arrays.toString(t.selectionSort(test)));//working
+        System.out.println(Arrays.toString(t.insertionSort(test)));//working
+        //System.out.println(t.mergeSort(test1));//working
+        //System.out.println(Arrays.toString(t.quickSort(test, 0, test.length - 1)));//working
 
     }
    public int[] bubbleSort(int[] arr){
@@ -29,9 +31,35 @@ public class Main {
        }
        return arr;
    }
-    public int[] quickSort(int[] arr){
-        //not implemented
+    public int[] quickSort(int[] arr,int low, int high){
+        if(low<high)
+        {
+            int index = partition(arr,low,high);
+            quickSort(arr,low,index-1);
+            quickSort(arr,index+1,high);
+        }
         return arr;
+    }
+
+    public int partition(int[] arr,int low,int high){
+
+        int pivotval=arr[low];
+
+        int wall=low;
+        for (int i = low+1; i <= high; i++) {
+            if(arr[i]<=pivotval){
+                wall++;
+                int s=arr[i];
+                arr[i]=arr[wall];
+                arr[wall]=s;
+
+            }
+        }
+        int s=arr[low];
+        arr[low]=arr[wall];
+        arr[wall]=s;
+
+        return wall;
     }
 
     //recursively sort each array
@@ -98,24 +126,25 @@ public class Main {
 
     }
     public int[] selectionSort(int[] arr){
-        for (int i = 0; i <arr.length-1 ; i++) {
-            int tmp=i;
-            for (int j = i+1; j <arr.length-1 ; j++) {
+        for (int i = 0; i <arr.length-1; i++) {
+            int min=i;
+            for (int j = i+1; j <arr.length ; j++) {
                 if(arr[j]<arr[i])
                 {
-                    tmp=j;
-                    arr[j]=arr[i];
-                    arr[i]=tmp;
+                    min=j;
+                    int tmp=arr[i];
+                    arr[i]=arr[min];
+                    arr[min]=tmp;
                 }
             }
         }
         return arr;
     }
     public int[] insertionSort(int[] arr){
-        for (int i = 1; i <arr.length-1 ; i++) {
+        for (int i = 1; i <arr.length; i++) {
             int tmp= arr[i];
             int piv=i;
-            while(i>0 && arr[piv-1]>=tmp){
+            while(piv>0 && arr[piv-1]>=tmp){
                 arr[piv]=arr[piv-1];
                 piv--;
             }
